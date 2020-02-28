47ed9069ade3afc7055a4f2293cce1e75f74f728
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-02-29 05:25:11 +0530

    key and value pairs corresponding to the Huffman tree related to each character inputted in the map,i.e. ,the object of HashMap

--- src/huffmanGenerator/HuffmanGenerator.java
+++ src/huffmanGenerator/HuffmanGenerator.java
@@ -26,6 +26,32 @@ public class HuffmanGenerator {
 
     public String convertNormalStringToHuffmanString() {
         HashMap<Character, String> map = new HashMap<>();
+        map.put('a', "1010");
+        map.put('b', "100000");
+        map.put('c', "00000");
+        map.put('d', "10110");
+        map.put('e', "010");
+        map.put('f', "110011");
+        map.put('g', "100010");
+        map.put('h', "0001");
+        map.put('i', "0110");
+        map.put('j', "1100001011");
+        map.put('k', "11000011");
+        map.put('l', "10111");
+        map.put('m', "110010");
+        map.put('n', "0111");
+        map.put('o', "1001");
+        map.put('p', "100001");
+        map.put('q', "1100001001");
+        map.put('r', "0010");
+        map.put('s', "0011");
+        map.put('t', "1101");
+        map.put('u', "00001");
+        map.put('v', "1100000");
+        map.put('w', "110001");
+        map.put('x', "1100001000");
+        map.put('y', "100011");
+        map.put('z', "1100001010");
         return "";
     }
 }
